e8bd56c168863593a9daf2ddcac073ae8e4fc389
LE VAN NHAT <95281824+nhatlv12@users.noreply.github.com>
2026-07-09 23:16:47 +0700

    Update environment.ts

--- App.Angular/src/environments/environment.ts
+++ App.Angular/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
-  apiUrl: 'http://192.168.1.20:8080/api',
-  appName: 'Web Bán Hàng Trang'
+  production: false,
+  apiUrl: 'http://localhost:8080/api',
+  appName: 'Web Bán Hàng Trang (Development)'
 };
